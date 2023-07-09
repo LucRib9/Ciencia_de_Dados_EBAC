539fe3d0d1732259a469aae3cde070253848c594
Lucas Ribeiro <127044748+LucRib9@users.noreply.github.com>
2023-07-09 20:44:18 -0300

    Add files via upload

---
+++ Módulo 15 - Tarefa/Códigos/Introdução.py
@@ -0,0 +1,26 @@
+# Ciência de Dados -  Exercício - Módulo 15 
+# Conceitos principais do pacote streamlit
+
+# Importação dos pacotes necessários para esta tarefa
+import streamlit as st # Responsável pela criação de páginas na web
+import pandas as pd # Leitura de dados e construção de tabelas
+import numpy as np # Possibilita diferentes operações e métodos matemáticos
+
+
+st.title("Curso de Ciência de Dados - EBAC ")
+st.title("Tarefa do Módulo 15")
+
+st.markdown("# Introdução")
+st.text(""" Nesta tarefa foi desenvolvido uma aplicação por meio do streamlit, na qual foram
+utilizados os códigos disponibilizados na documentação do streamlit. Dentre as pá-
+ginas presentes na documentação, destacam-se a dos conceitos principais, API 
+reference e a de criação de um app, que podem ser visitadas por meio dos links 
+abaixo:""")
+
+st.markdown("https://docs.streamlit.io/library/get-started/main-concepts")
+st.markdown("https://docs.streamlit.io/library/api-reference")
+st.markdown("https://docs.streamlit.io/library/get-started/create-an-app")
+
+st.text(""" Os exemplos utilizados podem ser visualizados através das páginas desta aplicação
+presentes na barra lateral ao lado.""")
+st.markdown(":point_left:")
